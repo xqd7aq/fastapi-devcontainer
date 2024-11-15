3a229f7ab0442c897b5d38de04ec291015e40648
xqd7aq <143450447+xqd7aq@users.noreply.github.com>
2024-11-15 16:35:40 +0000

    Completed Part 4

--- app/main.py
+++ app/main.py
@@ -1,17 +1,68 @@
-#!/usr/bin/env python3
+    #!/usr/bin/env python3
 
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 import json
 import os
+import mysql.connector
+from mysql.connector import Error
 
-api = FastAPI()
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "xqd7aq"
 
-@api.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Hello API"}
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
-@api.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    
+@app.get('/songs')
+def get_songs():
+    query = """
+    SELECT 
+        songs.title AS title,
+        songs.album AS album,
+        songs.artist AS artist,
+        songs.year AS year,
+        CONCAT('https://s3.amazonaws.com/your-bucket/', songs.file) AS file,
+        CONCAT('https://s3.amazonaws.com/your-bucket/', songs.image) AS image,
+        genres.genre AS genre
+    FROM 
+        songs
+    JOIN 
+        genres ON songs.genre = genres.genreid
+    ORDER BY 
+        songs.title;
+    """
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
